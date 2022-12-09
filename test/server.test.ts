943bb245b29581423c97101afd97b32459c79676
SondreB <sondre@outlook.com>
2022-12-10 00:09:30 +0100

    Add Content ID test

--- test/server.test.ts
+++ test/server.test.ts
@@ -1,4 +1,21 @@
 import test from 'ava';
 import { didNotFound, Server } from '../src/server.js';
 
+test('Validate the CID (Content ID) implementation', async (t) => {
+	const server = new Server();
+	t.assert(server != null);
 
+	const cid1 = await server.generateCid({ id: '1' });
+	const cid2 = await server.generateCid({ id: '1' });
+
+	t.assert(cid1.toString() == cid2.toString());
+
+	const cid3 = server.parseCid(cid1.toString());
+	t.assert(cid1.equals(cid3));
+
+	// CID(bafyreibmc4xb7biwqftfnpoaeiymjvucqogn2u5tnagrwdgzgcwxdidg3q)
+	// bafyreibmc4xb7biwqftfnpoaeiymjvucqogn2u5tnagrwdgzgcwxdidg3q
+
+	const cid4 = server.parseCid('bafyreibmc4xb7biwqftfnpoaeiymjvucqogn2u5tnagrwdgzgcwxdidg3q');
+	t.assert(cid1.equals(cid4));
+});
